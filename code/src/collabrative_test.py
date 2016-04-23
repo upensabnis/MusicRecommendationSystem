8d70a021f966ad6807c8abf1ba7f2f15e24c5069
Dheeraj <rach9569@colorado.edu>
2016-04-23 12:39:39 -0600

    Final testing file

--- code/src/collabrative_test.py
+++ code/src/collabrative_test.py
@@ -17,7 +17,7 @@ db = client.temp_database
 track = db.tracks
 
 db2=client.jam_database
-jam = db.jams
+jam = db2.jams
 cluster=db2.cluster
 user_gener=db2.user_gener
 
@@ -32,10 +32,12 @@ user_data={}
 for collection in user_gener.find():
 	user_data[collection.get('user_id')]=collection.get('track')
 
-user_id=0
-user_id=random.choice(user_data.keys())
-user_data_random[user_id]=user_data[user_id]
-print user_data_random
+for i in range(10):
+	values=[]
+	userId=random.choice(user_data.keys())
+	user_data_random[userId]=user_data[userId]
+
+#print user_data_random
 	
 user_loop=list(user_data_random.keys())   #gets all the usersId's
 
@@ -48,6 +50,27 @@ for user in user_loop:   #Changes the dic in the format userId:[values of tracks
 	user_data_random[user]=values
 	
 #print cluster_centroid
+
+artist_title_data={}
+
+for collection in jam.find():
+	artist_title_data[collection['title']]=[]
+
+for collection in jam.find():
+	newvalue=[]
+	newvalue.append(collection.get('artist'))
+	newvalue.append(collection.get('jam_id'))
+	#print newvalue
+	artist_title_data[collection['title']].append(newvalue)
+	
+print "Jam data is loaded"
+
+track_artists = {}
+for collection in track.find({},no_cursor_timeout=True):
+	newKey = collection['artist_name'].join(':').join(collection['title'])
+	track_artists[newKey] = collection['title']
+
+print "track data loaded"
 	
 def calc_correlation(centroid,userterm):  
 	N=len(centroid)
@@ -68,9 +91,18 @@ def calc_correlation(centroid,userterm):
 def get_user_songs(user):
 	user_songs_list={}
 	counter=1
+	artist_songs=[]
+	artist_songs=artist_title_data[user]
+	
+	for data in artist_songs:
+		user_songs_list[counter]=data[1]
+		counter=counter+1
+	
+	'''
 	for collection in jam.find({'title':user}):
 		user_songs_list[counter]=collection['jam_id']
-		counter=counter+1
+		counter=counter+1'''
+		
 	return user_songs_list
 
 
@@ -83,24 +115,35 @@ def gettopsongs(cluster_selection):
 		cluster_data[data['cluster']]=data['users']
 
 	cluster_users=cluster_data[cluster_selection]
+	#cluster_users=cluster_data[cluster_selection]
 	
-	titles=[]
-	artist=[]
+	#print "got cluster data"	
+	#print cluster_users
 	usertosongs={}
-	print "Inside get top songs"
+	#print "Inside get top songs"
 	
-	for user in cluster_users:			#This will get all the songs listen by each user in the cluster
+	artist_data=[]
+	titles=[]
+	artists=[]
+	
+	for user in cluster_users:
+		artists=artist_title_data[user]
+		for value in artists:
+			artist_data.append(value[0])
+			titles.append(value[1])
+
+	'''for user in cluster_users:			#This will get all the songs listen by each user in the cluster
 		for data in jam.find({'title':user}):
 			titles.append(data['jam_id'])
-			artist.append(data['artist'])
+			artists.append(data['artist'])'''
 	
 	'''for collection in jam.find():
 		if jam['title']==user:
 			titles.append(data['jam_id'])
 			artist.append(data['artist'])'''
 
-	print "Got the songs from the jams"
-	print len(titles),len(artist)
+	#print "Got the songs from the jams"
+	print len(titles),len(artist_data)
 
 	
 	songs_count={}
@@ -108,16 +151,13 @@ def gettopsongs(cluster_selection):
 	for title in titles:
 		songs_count[title]=0
 
-	track_artists = {}
-	for collection in track.find({},no_cursor_timeout=True):
-		newKey = collection['artist_name'].join(':').join(collection['title'])
-		track_artists[newKey] = collection['title']
 	
-	for title,artist in zip(titles,artist): #This will get the matching song from the tracks tables
+	
+	for t,a in zip(titles,artist_data): #This will get the matching song from the tracks tables
 		#for data in track.find({'title':title,'artist_name':artist}):
-		searchKey = artist.join(':').join(title)
+		searchKey = a.join(':').join(t)
 		if track_artists.get(searchKey ):	
-			songs_count[title]=songs_count[title]+1
+			songs_count[t]=songs_count[t]+1
 	'''
 	songs_count={}
 
@@ -131,56 +171,70 @@ def gettopsongs(cluster_selection):
 	
 
 	final_songs={}
-	for key,value in songs_count.items():
+	for key,value in songs_count.items(): #This will elimiate the songs with zero song count
 		if value!=0:
 			final_songs[key]=value
-			
+	
+	
 	return final_songs
 
 def getaccuracy(user_songs,top_songs):
 	count=0
 	for user_song in user_songs:
 		if user_song in top_songs:
-			print "Hit"
+			#print "Hit"
 			count=count+1
 	return count
 
 def cluster_calcuation():
+	start_time=time.time()
 	centroid_id=[]
 	clusters={}
 	centroid_id=list(cluster_centroid.keys())
+	count=1
 	for users in centroid_id:
-			clusters[users]=[]
+		clusters[users]=[]
 	for user in user_loop:
-			user_value=[]
-			user_value=user_data_random[user]
-			correlation_values=[]
-			max_value=0
-			for centroid_user in centroid_id:
-				centroid_value= cluster_centroid[centroid_user]
-				correlation_values.append(calc_correlation(centroid_value,user_value))
-			max_value=max(correlation_values)
-			clusters[centroid_id[correlation_values.index(max_value)]].append(user)
-			
-	user_centroid_belongs=0
-	top_songs={}
-	for centroid_user in centroid_id:
-		if len(clusters[centroid_user])==1:
-			user_centroid_belongs=centroid_user
-			print "centroid",user_centroid_belongs
-	for user in user_loop:
+		print "user",count
+		user_centroid_belongs=0
+		user_value=[]
+		user_value=user_data_random[user]
+		correlation_values=[]
+		max_value=0
+		top_songs={}
+		user_songs={}
+		for centroid_user in centroid_id:
+			centroid_value= cluster_centroid[centroid_user]
+			correlation_values.append(calc_correlation(centroid_value,user_value))
+		max_value=max(correlation_values)
+		clusters[centroid_id[correlation_values.index(max_value)]].append(user)
+		user_centroid_belongs=centroid_id[correlation_values.index(max_value)]
+		
+		'''for centroid_user in centroid_id:
+			if len(clusters[centroid_user])==1:
+				user_centroid_belongs=centroid_user
+				print "centroid",user_centroid_belongs'''
+		
 		user_songs=get_user_songs(user)
 	
-	print user_songs
-	top_songs=gettopsongs(user_centroid_belongs)
+		#print user_songs
+		print "centroid",user_centroid_belongs
+		
+		top_songs=gettopsongs(user_centroid_belongs)
 	
-	print top_songs
-	percentage=0
-	hits=0
-	hits=getaccuracy(list(user_songs.values()),list(top_songs.keys()))
-	print len(top_songs.keys())
-	percentage=((float(hits)/len(top_songs.keys()))*100)
-	print percentage
+		#print top_songs
+		percentage=0
+		hits=0
+		hits=getaccuracy(list(user_songs.values()),list(top_songs.keys()))
+		#print len(top_songs.keys())
+		if len(top_songs.keys())!=0:
+			percentage=((float(hits)/len(top_songs.keys()))*100)
+			print "Total hits are",hits
+			print "Percentage",percentage
+		else:
+			print "No Top songs"
+		count=count+1
+	print("--- %s seconds ---" % (time.time() - start_time))	
 	
 def main():
 	cluster_calcuation()
